0e37c2e69e0bbc8a0d1fdb0efafd0eb4207b99a2
Sebastian Forsell <sebastian.forsell@learnet.se>
2018-05-02 21:32:59 +0200

    bla

---
+++ Uppgift1/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+using Newtonsoft.Json;
+using Uppgift1.Models;
+using System.Net.Http;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Uppgift1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public  async Task<ActionResult> Index()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync("https://jsonplaceholder.typicode.com/posts/1");
+                string content = await response.Content.ReadAsStringAsync();
+                if (response.IsSuccessStatusCode)
+                {
+                    var model = JsonConvert.DeserializeObject<Dictionary<string, string>>(content);
+                    return View(model);
+                }
+                return Content("An error has occurred: " + content);
+            }
+        }
+    }
+}
---
+++ Uppgift1/Scripts/index.js
@@ -0,0 +1,9 @@
+﻿"use strict";
+$(document).ready(function () {
+    var keyDivValues = $("#keyDiv");
+    $("#keyDiv").click(function () {
+        if ($(window).width() < 600) {
+            alert(keyDivValues.html());
+        }
+    });
+});
